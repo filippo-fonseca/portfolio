94c8e7aa0e21d6ff242a4723ee95de5489d45970
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-12-23 13:20:17 -0600

    x

--- styles/index.ts
+++ styles/index.ts
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
-export const TextShadowDiv = styled.div`
-  text-shadow: 5px 5px 10px rgba(58, 97, 255, 0.6);
-`;
+export const TextShadowDiv = styled.div``;
 
 export const LinkText = styled.a`
   text-decoration: underline;
